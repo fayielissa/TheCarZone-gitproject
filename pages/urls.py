a5385c643c1aa94703c7a4e54c24244234754823
fayielissa <fmabhena2@gmail.com>
2021-11-30 00:40:10 +0200

    commit all changes done today

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('cars', views.cars, name='cars'),
+    path('services', views.services, name='services'),
+    path('contacts', views.contacts, name='contacts')
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/home.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def cars(request):
+    return render(request, 'pages/cars.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def contacts(request):
+    return render(request, 'pages/contacts.html')
